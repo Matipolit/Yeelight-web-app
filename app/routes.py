28d61df9c79695fded651c047ac49af8bc2bad8f
Matipolit <matipolit@gmail.com>
2020-05-14 19:13:35 +0200

    reorganisation of files

---
+++ app/routes.py
@@ -0,0 +1,58 @@
+from app import app
+
+from flask import render_template, request, abort, Flask
+from yeelight import *
+import json
+from collections import namedtuple
+import json
+
+bulbs = [Bulb('192.168.1.100'), Bulb('192.168.1.101'), Bulb('192.168.1.102')]
+
+
+# ROUTING/VIEW FUNCTIONS
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    # Renders index.html.
+    # bulb_values = {}
+    # it = 0
+    # for bulb in bulbs:
+    #     try:
+    #         data = bulb.get_properties()
+    #         bulb_values[str(it)] = data
+    #         it = it + 1
+    #     except Exception:
+    #         bulb_values[str(it)] = {'connected': None}
+    #         it = it + 1
+
+    return render_template('index.html', bulbs=bulbs)
+
+
+@app.route('/f/<lampID>', methods=['POST'])
+def flashLight(lampID):
+    try:
+        bulbs[int(lampID)].toggle()
+        return "OK"
+    except Exception as e:
+        return e.args.__str__()
+
+
+@app.route('/color/<lampID>', methods=['POST'])
+def color(lampID):
+    try:
+        colors = request.form
+        bulbs[int(lampID)].set_rgb(int(colors.get('r')), int(colors.get('g')), int(colors.get('b')))
+        return "OK"
+    except Exception as e:
+        return e.args.__str__()
+
+
+@app.route('/changeIntensity/<lampID>', methods=['POST'])
+def changeIntensity(lampID):
+    try:
+        bulbs[int(lampID)].set_brightness(int(request.form.get('intensity')))
+    except Exception as e:
+        return e.args.__str__()
+    return "OK"
